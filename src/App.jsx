4ce1e98efaffaa6dd3abaabb3680a932fc58611e
“[bridgettenawari]” <[chebetbri@gmail.com]>
2026-08-19 13:45:57 +0300

    Deleted sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import BookList from "./components/BookList/BookList";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-import SideBar from "./components/SideBar/SideBar";
 
 function App() {
 	return (
 		<>
 			<NavigationBar />
-			<SideBar />
 			<BookList />
 		</>
 	);
